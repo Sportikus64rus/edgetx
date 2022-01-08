b719b9d2ac51a76b5917023b21c652b68dcdb8f1
gagarinlg <malte@langermann.net>
2022-01-08 19:34:23 +0100

    The SPI flash is now available as a mass storage device via usb. with this command line ./lfs --read_size=256 --prog_size=256 -b=4096 --block_count=2048 -d /dev/<spi flash device> <mount point>
    using this:
    https://github.com/littlefs-project/littlefs-fuse

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -20,6 +20,7 @@
  */
 
 #include "opentx.h"
+#include "VirtualFS.h"
 #include "io/frsky_firmware_update.h"
 #include "hal/adc_driver.h"
 #include "timers_driver.h"
@@ -1312,6 +1313,7 @@ void opentxClose(uint8_t shutdown)
 #if defined(SDCARD)
   sdDone();
 #endif
+  VirtualFS::instance().stop();
 }
 
 void opentxResume()
@@ -1324,6 +1326,7 @@ void opentxResume()
   luaInitThemesAndWidgets();
 #endif
 
+  VirtualFS::instance().restart();
   storageReadAll();
 
 #if defined(COLORLCD)
